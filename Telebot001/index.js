c67f040c85ac902b7ddee7324914d67985c1ab77
Omar Mammadli <158719141+memmedliomer@users.noreply.github.com>
2024-05-16 12:54:51 -0700

    index.js

--- Telebot001/index.js
+++ Telebot001/index.js
@@ -25,21 +25,21 @@ const commands = ['/start', '/return', '/clear'];
 
 var letnow = {};
 
-const questions = [
+const questions9 = [
     {
-        text: 'İngilis dili fənnindən ümumilikdə 26 qapalı sualdan düzgün cavablarınızın sayını yazın.',
+        text: 'İngilis dili fənnindən 26 qapalı sualdan düzgün cavablarınızın sayını yazın.',
         maxValue: 26
     },
     {
-        text: 'İngilis dili fənnindən ümumilikdə 4 açıq sualdan düzgün cavablarınızın sayını yazın.',
+        text: 'İngilis dili fənnindən 4 açıq sualdan düzgün cavablarınızın sayını yazın.',
         maxValue: 4
     },
     {
-        text: 'Azərbaycan dili fənnindən ümumilikdə 26 qapalı sualdan düzgün cavablarınızın sayını yazın.',
+        text: 'Azərbaycan dili fənnindən 26 qapalı sualdan düzgün cavablarınızın sayını yazın.',
         maxValue: 26
     },
     {
-        text: 'Azərbaycan dili fənnindən ümumilikdə 4 açıq sualdan düzgün cavablarınızın sayını yazın.',
+        text: 'Azərbaycan dili fənnindən 4 açıq sualdan düzgün cavablarınızın sayını yazın.',
         maxValue: 4
     },
     {
@@ -56,22 +56,73 @@ const questions = [
     }
 ];
 
+const questions11 = [
+    {
+        text: 'Azərbaycan dili fənnindən 20 qapalı sualdan düzgün cavablarınızın sayını yazın.',
+        maxValue: 20
+    },
+    {
+        text: 'Azərbaycan dili fənnindən 10 açıq sualdan düzgün cavablarınızın sayını yazın.',
+        maxValue: 10
+    },
+    {
+        text: 'Riyaziyyat fənnindən 13 qapalı sualdan düzgün cavablarınızın sayını yazın.',
+        maxValue: 13
+    },
+    {
+        text: 'Riyaziyyat fənnindən 5 kodlaşdırıla bilən sualdan düzgün cavablarınızın sayını yazın.',
+        maxValue: 5
+    },
+    {
+        text: 'Riyaziyyat fənnindən 7 tam açıq sualdan düzgün cavablarınızın sayını yazın.',
+        maxValue: 7
+    },
+    {
+        text: 'İngilis dili fənnindən 23 qapalı sualdan düzgün cavablarınızın sayını yazın.',
+        maxValue: 23
+    },
+    {
+        text: 'İngilis dili fənnindən 7 açıq sualdan düzgün cavablarınızın sayını yazın.',
+        maxValue: 7
+    }
+];
+
 // Dictionary to store exam status for each user
 var examStatus = {};
 
-// Function to calculate English score
-function calculateScore(closedQuestions, openQuestions) {
+// Function to calculate English score for 9th grade
+function calculateScore9(closedQuestions, openQuestions) {
     return (((openQuestions * 2) + closedQuestions) * 100) / 34;
 }
 
-// Function to calculate Mathematics score
-function calculateMathScore(nQ, nAK, nA) {
+// Function to calculate Azerbaijani score for 11th grade
+function calculateAzerbaijaniScore11(ty, th) {
+    return (2.5 * ((2 * th) + ty));
+}
+
+// Function to calculate Mathematics score for 9th grade
+function calculateMathScore9(nQ, nAK, nA) {
     return (((nA * 2) + nAK + nQ) * 100) / 29;
 }
 
-// Function to calculate total score
-function calculateTotalScore(englishScore, azerbaijaniScore, mathScore) {
+// Function to calculate Mathematics score for 11th grade
+function calculateMathScore11(rf, gh, fg) {
+    return ((25 / 8) * ((2 * fg) + rf + gh));
+}
+
+// Function to calculate English score for 11th grade
+function calculateEnglishScore11(rd, jh) {
+    return ((100 / 37) * ((2 * jh) + rd));
+}
+
+// Function to calculate total score for 9th grade
+function calculateTotalScore9(englishScore, azerbaijaniScore, mathScore) {
     return englishScore + azerbaijaniScore + mathScore;
+}
+
+// Function to calculate total score for 11th grade
+function calculateTotalScore11(azerbaijaniScore, mathScore, englishScore) {
+    return azerbaijaniScore + mathScore + englishScore;
 }
 
 // Function to validate input
@@ -136,16 +187,12 @@ bot.on('message', (msg) => {
     }
     messageIds[chatId].push(msg.message_id);
 
-    if (msg.text == '9' && letnow[chatId] === undefined) {
+    if ((msg.text == '9' || msg.text == '11') && letnow[chatId] === undefined) {
         bot.sendMessage(chatId, 'Zəhmət olmasa adınızı və soyadınızı yazın.').then((sentMsg) => {
             messageIds[chatId].push(sentMsg.message_id);
         });
-        letnow[chatId] = [0, 9]; // Stage 0 indicates asking for name and surname
-        users[chatId] = { answers: [] }; // Initialize user's data structure
-    } else if (msg.text == '11' && letnow[chatId] === undefined) {
-        bot.sendMessage(chatId, 'Hal-hazırda bu xidmətin aktiv olması üçün işlər görülür').then((sentMsg) => {
-            messageIds[chatId].push(sentMsg.message_id);
-        });
+        letnow[chatId] = [0, msg.text]; // Stage 0 indicates asking for name and surname
+        users[chatId] = { answers: [], grade: msg.text }; // Initialize user's data structure with grade
     } else {
         if (msg.text == '/start') {
             delete users[chatId];
@@ -160,7 +207,7 @@ bot.on('message', (msg) => {
             let stage = letnow[chatId][0];
             if (stage > 1) {
                 letnow[chatId][0] = stage - 1;
-                bot.sendMessage(chatId, `Əvvəlki suala qayıtdınız.Cavabınızı dəyişdirin.\n${questions[stage - 2].text}`).then((sentMsg) => {
+                bot.sendMessage(chatId, `Əvvəlki suala qayıtdınız.Cavabınızı dəyişdirin.\n${letnow[chatId][1] === '9' ? questions9[stage - 2].text : questions11[stage - 2].text}`).then((sentMsg) => {
                     messageIds[chatId].push(sentMsg.message_id);
                 });
             } else {
@@ -174,7 +221,7 @@ bot.on('message', (msg) => {
             if (stage === 0) { // Asking for name and surname
                 if (validateNameSurname(msg.text)) {
                     users[chatId].nameSurname = msg.text.trim();
-                    bot.sendMessage(chatId, questions[0].text).then((sentMsg) => {
+                    bot.sendMessage(chatId, letnow[chatId][1] === '9' ? questions9[0].text : questions11[0].text).then((sentMsg) => {
                         messageIds[chatId].push(sentMsg.message_id);
                     });
                     letnow[chatId][0] = 1; // Move to next stage
@@ -185,24 +232,34 @@ bot.on('message', (msg) => {
                 }
             } else if (stage !== undefined) {
                 let quiz = letnow[chatId];
-                let num = parseInt(msg.text);
+                let num = parseFloat(msg.text);
 
                 if (commands.indexOf(msg.text) == -1) {
-                    if (validateInput(num, questions[quiz[0] - 1].maxValue)) {
+                    if (validateInput(num, (quiz[1] === '9' ? questions9[quiz[0] - 1].maxValue : questions11[quiz[0] - 1].maxValue))) {
                         users[chatId].answers.push(num);
-                        if (quiz[0] === questions.length) {
-                            const eng = parseFloat(calculateScore(users[chatId].answers[0], users[chatId].answers[1]).toFixed(2));
-                            const az = parseFloat(calculateScore(users[chatId].answers[2], users[chatId].answers[3]).toFixed(2));
-                            const math = parseFloat(calculateMathScore(users[chatId].answers[4], users[chatId].answers[5], users[chatId].answers[6]).toFixed(2));
-                            const total = parseFloat(calculateTotalScore(eng, az, math).toFixed(2));
-                            bot.sendMessage(chatId, `${users[chatId].nameSurname}\n\nİngilis dili: ${eng}\nAzərbaycan dili: ${az}\nRiyaziyyat: ${math}\n\nSizin yekun nəticəniz: ${total}`).then((sentMsg) => {
-                                messageIds[chatId].push(sentMsg.message_id);
-                            });
+                        if (quiz[0] === (quiz[1] === '9' ? questions9.length : questions11.length)) {
+                            if (quiz[1] === '9') {
+                                const eng = parseFloat(calculateScore9(users[chatId].answers[0], users[chatId].answers[1]).toFixed(2));
+                                const az = parseFloat(calculateScore9(users[chatId].answers[2], users[chatId].answers[3]).toFixed(2));
+                                const math = parseFloat(calculateMathScore9(users[chatId].answers[4], users[chatId].answers[5], users[chatId].answers[6]).toFixed(2));
+                                const total = parseFloat(calculateTotalScore9(eng, az, math).toFixed(2));
+                                bot.sendMessage(chatId, `${users[chatId].nameSurname}\n\nİngilis dili: ${eng}\nAzərbaycan dili: ${az}\nRiyaziyyat: ${math}\n\nSizin yekun nəticəniz: ${total}`).then((sentMsg) => {
+                                    messageIds[chatId].push(sentMsg.message_id);
+                                });
+                            } else if (quiz[1] === '11') {
+                                const az = parseFloat(calculateAzerbaijaniScore11(users[chatId].answers[0], users[chatId].answers[1]).toFixed(2));
+                                const math = parseFloat(calculateMathScore11(users[chatId].answers[2], users[chatId].answers[3], users[chatId].answers[4]).toFixed(2));
+                                const eng = parseFloat(calculateEnglishScore11(users[chatId].answers[5], users[chatId].answers[6]).toFixed(2));
+                                const total = parseFloat(calculateTotalScore11(az, math, eng).toFixed(2));
+                                bot.sendMessage(chatId, `${users[chatId].nameSurname}\n\nAzərbaycan dili: ${az}\nRiyaziyyat: ${math}\nİngilis dili: ${eng}\n\nSizin yekun nəticəniz: ${total}`).then((sentMsg) => {
+                                    messageIds[chatId].push(sentMsg.message_id);
+                                });
+                            }
                             delete users[chatId];
                             delete letnow[chatId];
                             return;
                         }
-                        bot.sendMessage(chatId, questions[quiz[0]].text).then((sentMsg) => {
+                        bot.sendMessage(chatId, quiz[1] === '9' ? questions9[quiz[0]].text : questions11[quiz[0]].text).then((sentMsg) => {
                             messageIds[chatId].push(sentMsg.message_id);
                         });
                         letnow[chatId][0] = quiz[0] + 1;
